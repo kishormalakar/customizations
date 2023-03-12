89a714e170c41d733cc560dc6bc43f0069dcc49d
Kishor Malakar <19328750+kishormalakar@users.noreply.github.com>
2023-03-12 11:55:35 +0530

    Update eoffice-scripts.user.js

--- eoffice-scripts.user.js
+++ eoffice-scripts.user.js
@@ -9,10 +9,16 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+(function () {
+	"use strict";
 
-    var inboxListDiv = document.querySelectorAll('.inboxListDiv')[0];
-    inboxListDiv.parentElement.classList.add('inboxListDivParent');
+	var inboxListDiv = document.querySelectorAll(".inboxListDiv")[0];
+	inboxListDiv.parentElement.classList.add("inboxListDivParent");
 
+	var advancedSearch = document.querySelectorAll('[id^="formGlobalSearch"]')[0];
+	var diaryDate = advancedSearch.querySelectorAll('input[value="6"]')[0];
+	var letterDate = advancedSearch.querySelectorAll('input[value="7"]')[0];
+
+	diaryDate.checked = true;
+	letterDate.checked = false;
 })();
