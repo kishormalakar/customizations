f0cc610feb469a4d3634ef3096566b91234862c8
Kishor Malakar <19328750+kishormalakar@users.noreply.github.com>
2023-03-24 23:19:26 +0530

    rtyuio

--- immis-scripts.user.js
+++ immis-scripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IMMIS
 // @namespace    http://tampermonkey.net/
-// @version      1.0.4
+// @version      1.0.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://ireps.gov.in/fcgi/*
@@ -87,7 +87,7 @@ window.addEventListener(
 		}
 
 		if (document.title == "Purchase Order Generation" || document.title == "Run Form - IMMIS/PUR/ORDER") {
-			canvasHolder.classList.add("po_modification");
+			canvasHolder.classList.add("po_generation");
 		}
 
 		if (document.title == "Publish Tender Document" || document.title == "Run Form - IMMIS/PUR/TENDERNEW") {
@@ -135,6 +135,91 @@ window.addEventListener(
 							consigneeRow.style.display = "none";
 						}
 					}
+				}
+			});
+		}
+
+		if (document.title == "Vendor Performance" || document.title == "Run Form - IMMIS/PUR/VENDPOS") {
+			canvasHolder.classList.add("vendor_performance");
+
+			document.addEventListener("click", (e) => {
+				if (e.target.name == "btn_Show_0") {
+					var s_2 = document.querySelectorAll("#s_2")[0];
+					var table = s_2.querySelectorAll("tbody")[0];
+
+					if (!e.target.closest("tr").nextSibling) {
+						var tr1 = document.createElement("tr");
+						var td1 = document.createElement("td");
+						var text1 = document.createTextNode("Total Orders");
+						td1.appendChild(text1);
+						var td2 = document.createElement("td");
+						tr1.appendChild(td1);
+						tr1.appendChild(td2);
+						table.appendChild(tr1);
+
+						var tr2 = document.createElement("tr");
+						var td3 = document.createElement("td");
+						var text2 = document.createTextNode("Partly Supplied");
+						td3.appendChild(text2);
+						var td4 = document.createElement("td");
+						tr2.appendChild(td3);
+						tr2.appendChild(td4);
+						table.appendChild(tr2);
+
+						var tr3 = document.createElement("tr");
+						var td5 = document.createElement("td");
+						var text3 = document.createTextNode("Fully Supplied");
+						td5.appendChild(text3);
+						var td6 = document.createElement("td");
+						tr3.appendChild(td5);
+						tr3.appendChild(td6);
+						table.appendChild(tr3);
+					}
+
+					var numTotalPO = 0;
+					var numPCPO = 0;
+					var numFCPO = 0;
+
+					var poQty = 0;
+					var cancelledQty = 0;
+					var suppliedQty = 0;
+
+					var s_3 = document.querySelectorAll("#s_3")[0];
+					var rows =
+						s_3.children[2].children[0].children[1].querySelectorAll("table")[1].children[0].children;
+
+					for (var i = 1; i < rows.length; i = i + 2) {
+						var row = rows[i];
+						numTotalPO++;
+
+						if (row.children.length == 9) {
+							poQty = row.children[3].innerText.split(" ")[0];
+							cancelledQty = row.children[4].innerText == "-" ? 0 : row.children[4].innerText;
+							suppliedQty = row.children[5].innerText == "-" ? 0 : row.children[5].innerText;
+						}
+
+						if (row.children.length == 8) {
+							poQty = row.children[2].innerText.split(" ")[0];
+							cancelledQty = row.children[3].innerText == "-" ? 0 : row.children[3].innerText;
+							suppliedQty = row.children[4].innerText == "-" ? 0 : row.children[4].innerText;
+						}
+
+						if (+suppliedQty > 0 && +suppliedQty < 0.95 * +poQty) {
+							numPCPO++;
+						}
+
+						if (+suppliedQty >= 0.95 * +poQty) {
+							numFCPO++;
+						}
+					}
+
+					e.target.closest("tr").nextElementSibling.querySelectorAll("td")[1].innerText = numTotalPO;
+					e.target.closest("tr").nextElementSibling.nextElementSibling.querySelectorAll("td")[1].innerText =
+						numPCPO;
+					e.target
+						.closest("tr")
+						.nextElementSibling.nextElementSibling.nextElementSibling.querySelectorAll("td")[1].innerText =
+						numFCPO;
 				}
 			});
 		}
