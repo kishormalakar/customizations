79536c9bb23c2514cb676ecbc98a2aae71f2da41
Kishor Malakar <19328750+kishormalakar@users.noreply.github.com>
2023-06-07 00:49:15 +0530

    adad

--- udm-scripts.user.js
+++ udm-scripts.user.js
@@ -14,4 +14,7 @@
 
 	var sideNavBar = document.querySelectorAll("#sidenavbar")[0];
 	sideNavBar.style.marginLeft = "-250px";
+
+	/*sideNavBar.querySelectorAll("#nabbar_close")[0].style.display = "none";
+    sideNavBar.querySelectorAll("#nabbar_open")[0].style.display = "inline-block";*/
 })();
