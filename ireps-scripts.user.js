0a372b28960fd46e3549b6271f15d1d347daf344
Kishor Malakar <19328750+kishormalakar@users.noreply.github.com>
2023-10-14 21:03:53 +0530

    Update ireps-scripts.user.js

--- ireps-scripts.user.js
+++ ireps-scripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IREPS
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://ireps.gov.in/epsn/*
